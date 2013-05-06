e47b2ec039dfe4da3b24fd9b91fa6cff5f1a7490
osm0sis <osm0sis@outlook.com>
2013-05-06 01:22:46 -0300

    v1.5 - multiple compression support - added a manual extraction of the ramdiskaddr from the supplied image header since unpackbootimg doesn't split this info out to file on its own. Should also widen support to other devices that are picky about their ramdiskaddr. - support for all the major compression algorithms/containers used for ramdisks(!): gzip, lzop, lzma, xz & bzip2; gzip is the standard and most common, but some developers use others in an attempt to improve boot times. The type of compression is determined and written out to file (ramdiskcomp), then used to unpack appropriately. Repacking is done with the same compression used by the original image.

--- repackimg.bat
+++ repackimg.bat
@@ -15,7 +15,18 @@ echo.
 :nowarning
 echo Packing ramdisk . . .
 echo.
-%bin%\mkbootfs ramdisk | %bin%\gzip > ramdisk-new.cpio.gz
+for /f "delims=" %%a in ('dir /b split_img\*-ramdiskcomp') do @set ramdiskcname=%%a
+for /f "delims=" %%a in ('type split_img\%ramdiskcname%') do @set ramdiskcomp=%%a
+if "%ramdiskcomp%"=="LZMA" set "ramdiskcomp=lzma"
+if "%ramdiskcomp%"=="XZ" set "ramdiskcomp=xz"
+echo Using compression: %ramdiskcomp%
+if "%ramdiskcomp%"=="gzip" %bin%\mkbootfs ramdisk | %bin%\gzip > ramdisk-new.cpio.gz
+if "%ramdiskcomp%"=="lzop" %bin%\mkbootfs ramdisk | %bin%\lzop > ramdisk-new.cpio.lzo
+if "%ramdiskcomp%"=="lzma" %bin%\mkbootfs ramdisk | %bin%\xz -Flzma > ramdisk-new.cpio.lzma
+if "%ramdiskcomp%"=="xz" %bin%\mkbootfs ramdisk | %bin%\xz -1 -Ccrc32 > ramdisk-new.cpio.xz
+if "%ramdiskcomp%"=="bzip2" %bin%\mkbootfs ramdisk | %bin%\bzip2 > ramdisk-new.cpio.bz2
+for /f "delims=" %%a in ('dir /b ramdisk-new.cpio.*') do @set ramdisk=%%a
+echo.
 
 echo Getting build information . . .
 echo.
@@ -30,6 +41,9 @@ echo base = %base%
 for /f "delims=" %%a in ('dir /b split_img\*-pagesize') do @set pagename=%%a
 for /f "delims=" %%a in ('type split_img\%pagename%') do @set pagesize=%%a
 echo pagesize = %pagesize%
+for /f "delims=" %%a in ('dir /b split_img\*-ramdiskaddr') do @set ramdiskaname=%%a
+for /f "delims=" %%a in ('type split_img\%ramdiskaname%') do @set ramdiskaddr=%%a0000
+echo ramdiskaddr = %ramdiskaddr%
 echo.
 
 echo Building image . . .
@@ -37,10 +51,10 @@ echo.
 setlocal EnableDelayedExpansion
 if "!cmdline!"=="" endlocal & goto nocmd
 endlocal
-%bin%\mkbootimg --kernel split_img/%kernel% --ramdisk ramdisk-new.cpio.gz --cmdline %cmdline% --base %base% --pagesize %pagesize% -o image-new.img
+%bin%\mkbootimg --kernel split_img/%kernel% --ramdisk %ramdisk% --cmdline %cmdline% --base %base% --pagesize %pagesize% --ramdiskaddr %ramdiskaddr% -o image-new.img
 goto done
 :nocmd
-%bin%\mkbootimg --kernel split_img/%kernel% --ramdisk ramdisk-new.cpio.gz --base %base% --pagesize %pagesize% -o image-new.img
+%bin%\mkbootimg --kernel split_img/%kernel% --ramdisk %ramdisk% --base %base% --pagesize %pagesize% --ramdiskaddr %ramdiskaddr% -o image-new.img
 
 :done
 echo Done!
--- unpackimg.bat
+++ unpackimg.bat
@@ -26,13 +26,30 @@ echo Splitting image to "/split_img/" . . .
 echo.
 cd split_img
 %bin%\unpackbootimg -i "%~p0%~n1%~x1"
+%bin%\dd ibs=1 count=2 skip=22 obs=1 if="%~p0%~n1%~x1" 2>nul | %bin%\od -h | %bin%\head -n 1 | %bin%\cut -c 9- > %~n1%~x1-ramdiskaddr
+for /f "delims=" %%a in ('dir /b *-ramdiskaddr') do @set ramdiskaname=%%a
+for /f "delims=" %%a in ('type %ramdiskaname%') do @set ramdiskaddr=%%a0000
+echo BOARD_RAMDISK_ADDR %ramdiskaddr%
 echo.
+%bin%\file -m %bin%\magic "%~n1%~x1-ramdisk.gz" 2>nul | %bin%\cut -d: -f2 | %bin%\cut -d" " -f2 > %~n1%~x1-ramdiskcomp
+for /f "delims=" %%a in ('dir /b *-ramdiskcomp') do @set ramdiskcname=%%a
+for /f "delims=" %%a in ('type %ramdiskcname%') do @set ramdiskcomp=%%a
+if "%ramdiskcomp%"=="gzip" ren "%~n1%~x1-ramdisk.gz" "%~n1%~x1-ramdisk.cpio.gz"
+if "%ramdiskcomp%"=="lzop" ren "%~n1%~x1-ramdisk.gz" "%~n1%~x1-ramdisk.cpio.lzo"
+if "%ramdiskcomp%"=="LZMA" set "ramdiskcomp=lzma" & ren "%~n1%~x1-ramdisk.gz" "%~n1%~x1-ramdisk.cpio.lzma"
+if "%ramdiskcomp%"=="XZ" set "ramdiskcomp=xz" & ren "%~n1%~x1-ramdisk.gz" "%~n1%~x1-ramdisk.cpio.xz"
+if "%ramdiskcomp%"=="bzip2" ren "%~n1%~x1-ramdisk.gz" "%~n1%~x1-ramdisk.cpio.bz2"
 cd ..
 
 echo Unpacking ramdisk to "/ramdisk/" . . .
 echo.
 cd ramdisk
-%bin%\gzip -dc "../split_img/%~n1%~x1-ramdisk.gz" | %bin%\cpio -i
+echo Compression used: %ramdiskcomp%
+if "%ramdiskcomp%"=="gzip" %bin%\gzip -dc "../split_img/%~n1%~x1-ramdisk.cpio.gz" | %bin%\cpio -i
+if "%ramdiskcomp%"=="lzop" %bin%\lzop -dc "../split_img/%~n1%~x1-ramdisk.cpio.lzo" | %bin%\cpio -i
+if "%ramdiskcomp%"=="lzma" %bin%\xz -dc "../split_img/%~n1%~x1-ramdisk.cpio.lzma" | %bin%\cpio -i
+if "%ramdiskcomp%"=="xz" %bin%\xz -dc "../split_img/%~n1%~x1-ramdisk.cpio.xz" | %bin%\cpio -i
+if "%ramdiskcomp%"=="bzip2" %bin%\bzip2 -dc "../split_img/%~n1%~x1-ramdisk.cpio.bz2" | %bin%\cpio -i
 echo.
 cd ..
 
