f6488f18e9298b81df53f8e79d8dafdce275c4b4
osm0sis <osm0sis@outlook.com>
2015-03-25 22:44:07 -0300

    v2.2 - user control of repacked ramdisk compression level - update slimmed magic file (from file 5.21) with my own fixes. - add advanced repackimg --level commandline option to control the compression level of the repacked ramdisk.

--- repackimg.bat
+++ repackimg.bat
@@ -6,7 +6,6 @@ cd "%~p0"
 if not exist split_img\nul goto nofiles
 if not exist ramdisk\nul goto nofiles
 set bin=android_win_tools
-set "args=%*"
 set "errout= "
 if "%hideErrors%" == "y" set "errout=2>nul"
 
@@ -20,18 +19,27 @@ echo.
 
 :nowarning
 del ramdisk-new.cpio* 2>nul
-if "%args%" == "--original" echo Repacking with original ramdisk . . . & goto skipramdisk
+if "%1" == "--original" echo Repacking with original ramdisk . . . & goto skipramdisk
 echo Packing ramdisk . . .
 echo.
 for /f "delims=" %%a in ('dir /b split_img\*-ramdiskcomp') do @set ramdiskcname=%%a
 for /f "delims=" %%a in ('type "split_img\%ramdiskcname%"') do @set ramdiskcomp=%%a
-echo Using compression: %ramdiskcomp%
-if "%ramdiskcomp%" == "gzip" set "repackcmd=gzip" & set "compext=gz"
-if "%ramdiskcomp%" == "lzop" set "repackcmd=lzop" & set "compext=lzo"
-if "%ramdiskcomp%" == "lzma" set "repackcmd=xz -Flzma" & set "compext=lzma"
-if "%ramdiskcomp%" == "xz" set "repackcmd=xz -1 -Ccrc32" & set "compext=xz"
-if "%ramdiskcomp%" == "bzip2" set "repackcmd=bzip2" & set "compext=bz2"
-if "%ramdiskcomp%" == "lz4" set "repackcmd=lz4 stdin stdout 2>nul" & set "compext=lz4"
+
+if "%1" == "--level" if not [%2] == [] (
+  if "%ramdiskcomp%" == "lz4" ( set "level=-c%2" ) else ( set "level=-%2" )
+  set "lvltxt= - Level: %2"
+) else (
+  set lvltxt=
+  set level=
+  if "%ramdiskcomp%" == "xz" set "level=-1"
+)
+echo Using compression: %ramdiskcomp%%lvltxt%
+if "%ramdiskcomp%" == "gzip" set "repackcmd=gzip %level%" & set "compext=gz"
+if "%ramdiskcomp%" == "lzop" set "repackcmd=lzop %level%" & set "compext=lzo"
+if "%ramdiskcomp%" == "lzma" set "repackcmd=xz -Flzma %level%" & set "compext=lzma"
+if "%ramdiskcomp%" == "xz" set "repackcmd=xz %level% -Ccrc32" & set "compext=xz"
+if "%ramdiskcomp%" == "bzip2" set "repackcmd=bzip2 %level%" & set "compext=bz2"
+if "%ramdiskcomp%" == "lz4" set "repackcmd=lz4 %level% stdin stdout 2>nul" & set "compext=lz4"
 %bin%\mkbootfs ramdisk %errout% | %bin%\%repackcmd% %errout% > ramdisk-new.cpio.%compext%
 if errorlevel == 1 goto error
 :skipramdisk
@@ -42,7 +50,8 @@ echo.
 for /f "delims=" %%a in ('dir /b split_img\*-zImage') do @set kernel=%%a
 echo kernel = %kernel%
 for /f "delims=" %%a in ('dir /b split_img\*-ramdisk.cpio*') do @set ramdisk=%%a
-if "%args%" == "--original" echo ramdisk = %ramdisk% & set "ramdisk=--ramdisk "split_img/%ramdisk%""
+if "%1" == "--original" echo ramdisk = %ramdisk% & set "ramdisk=--ramdisk "split_img/%ramdisk%""
+if not "%1" == "--original" set "ramdisk=--ramdisk ramdisk-new.cpio.%compext%"
 for /f "delims=" %%a in ('dir /b split_img\*-cmdline') do @set cmdname=%%a
 for /f "delims=" %%a in ('type "split_img\%cmdname%"') do @set cmdline=%%a
 echo cmdline = %cmdline%
@@ -79,7 +88,6 @@ echo.
 
 echo Building image . . .
 echo.
-if not "%args%" == "--original" set "ramdisk=--ramdisk ramdisk-new.cpio.%compext%"
 %bin%\mkbootimg --kernel "split_img/%kernel%" %ramdisk% %second% --cmdline "%cmdline%" --board "%board%" --base %base% --pagesize %pagesize% --kernel_offset %kerneloff% --ramdisk_offset %ramdiskoff% %second_offset% --tags_offset %tagsoff% %dtb% -o image-new.img %errout%
 if errorlevel == 1 goto error
 
